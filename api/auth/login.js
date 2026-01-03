1ad19112a64c3e1c4b5e401c1b7208b2499039ac
Senaxsss <musstdunichtwissenok@gmail.com>
2026-01-03 18:56:27 +0100

    Create login.js

---
+++ api/auth/login.js
@@ -0,0 +1,12 @@
+export default function handler(req, res) {
+  const params = new URLSearchParams({
+    client_id: process.env.DISCORD_CLIENT_ID,
+    redirect_uri: process.env.DISCORD_REDIRECT_URI,
+    response_type: "code",
+    scope: "identify email"
+  });
+
+  res.redirect(
+    "https://discord.com/api/oauth2/authorize?" + params
+  );
+}
